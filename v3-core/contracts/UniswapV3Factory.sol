bafdd1da2b19f6bf0c7c99a7ca19829b2918d7d7
WangRenjun <wangrenjun@github.com>
2022-06-15 14:32:28 +0800

    uniswap v3 research note

--- v3-core/contracts/UniswapV3Factory.sol
+++ v3-core/contracts/UniswapV3Factory.sol
@@ -25,11 +25,11 @@ contract UniswapV3Factory is IUniswapV3Factory, UniswapV3PoolDeployer, NoDelegat
         owner = msg.sender;
         emit OwnerChanged(address(0), msg.sender);
 
-        feeAmountTickSpacing[500] = 10;
+        feeAmountTickSpacing[500] = 10; //万五手续费 跳过10个刻度,
         emit FeeAmountEnabled(500, 10);
-        feeAmountTickSpacing[3000] = 60;
+        feeAmountTickSpacing[3000] = 60; //千三手续费 跳过60个刻度,
         emit FeeAmountEnabled(3000, 60);
-        feeAmountTickSpacing[10000] = 200;
+        feeAmountTickSpacing[10000] = 200; //百一手续费 跳过200个刻度,
         emit FeeAmountEnabled(10000, 200);
     }
 
